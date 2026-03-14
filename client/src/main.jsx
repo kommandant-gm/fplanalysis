a948bad07fce47991fe61defc24ac34b098e826a
kommandant-gm <49630340+kommandant-gm@users.noreply.github.com>
2026-03-14 16:13:42 +0800

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || '';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
